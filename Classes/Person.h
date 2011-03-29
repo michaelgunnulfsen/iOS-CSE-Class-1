5736e42a36fe184f69df7d5a59e671c43d3e1684
michael gunnulfsen <michaelgunnulfsen@michael-gunnulfsens-MacBook-Pro.local>
2011-03-29 15:39:54 +1100

    added person.

---
+++ Classes/Person.h
@@ -0,0 +1,16 @@
+//
+//  Person.h
+//  Class1
+//
+//  Created by michael gunnulfsen on 3/29/11.
+//  Copyright 2011 university of oslo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Person : NSObject {
+
+}
+
+@end
---
+++ Classes/Person.m
@@ -0,0 +1,14 @@
+//
+//  Person.m
+//  Class1
+//
+//  Created by michael gunnulfsen on 3/29/11.
+//  Copyright 2011 university of oslo. All rights reserved.
+//
+
+#import "Person.h"
+
+
+@implementation Person
+
+@end
